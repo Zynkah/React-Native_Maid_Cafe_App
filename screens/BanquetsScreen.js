b2143738675db160cdc168f4d73e2fb96c0ab713
Zynkah <ffgurl07@yahoo.com>
2022-11-18 19:17:42 -0800

    Banquet Rooms Added

--- screens/BanquetsScreen.js
+++ screens/BanquetsScreen.js
@@ -1,10 +1,29 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, ScrollView } from "react-native";
+import { Card, ListItem, Avatar } from "react-native-elements";
+import { BANQUETS } from "../shared/BANQUETS";
 
 const BanquetsScreen = () => {
+  const [banquets, setBanquets] = useState(BANQUETS);
   return (
-    <View>
-      <Text>Banquet Screen</Text>
-    </View>
+    <ScrollView>
+      <Card>
+        <Card.Title>Banquet Rooms</Card.Title>
+        <Card.Divider />
+        {banquets.map((banquet) => {
+          return (
+            <ListItem key={banquet.id}>
+              <Avatar source={banquet.image} size={100} />
+              <ListItem.Content>
+                <ListItem.Title>{banquet.name}</ListItem.Title>
+                <ListItem.Subtitle>{banquet.description}</ListItem.Subtitle>
+                <ListItem.Subtitle>{banquet.price}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          );
+        })}
+      </Card>
+    </ScrollView>
   );
 };
 export default BanquetsScreen;
