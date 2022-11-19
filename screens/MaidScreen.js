d5ac467b76b2af6b0d9e32f6bf65e8e74956047b
Zynkah <ffgurl07@yahoo.com>
2022-11-18 19:00:42 -0800

    Maids Page Updated

--- screens/MaidScreen.js
+++ screens/MaidScreen.js
@@ -1,10 +1,37 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Text, ScrollView } from "react-native";
+import { Card, ListItem, Avatar } from "react-native-elements";
+import { MAIDS } from "../shared/MAIDS";
 
 const MaidScreen = () => {
+  const [maids, setMaids] = useState(MAIDS);
   return (
-    <View>
-      <Text>Maid Screen</Text>
-    </View>
+    <ScrollView>
+      <Card wrapperStyle={{ margin: 20 }}>
+        <Card.Title>Our Mission</Card.Title>
+        <Card.Divider />
+        <Text>
+          Maid cafes focus on providing an escape from the home and work
+          spheres. Maids encapsulate a more pure form that provides an alternate
+          world to patrons.
+        </Text>
+      </Card>
+      <Card>
+        <Card.Title>Our Maids</Card.Title>
+        <Card.Divider />
+        {maids.map((maid) => {
+          return (
+            <ListItem key={maid.id}>
+              <Avatar source={maid.image} size={100} />
+              <ListItem.Content>
+                <ListItem.Title>{maid.name}</ListItem.Title>
+                <ListItem.Subtitle>{maid.description}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          );
+        })}
+      </Card>
+    </ScrollView>
   );
 };
 export default MaidScreen;
