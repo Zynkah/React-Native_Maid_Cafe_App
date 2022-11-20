6cbe8adbb5f3ac13d5eeec35c3a4c4e501c7f7ed
Zynkah <ffgurl07@yahoo.com>
2022-11-19 17:59:06 -0800

    Cart Started

--- features/menu/RenderMenu.js
+++ features/menu/RenderMenu.js
@@ -1,10 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Card } from "react-native-elements";
+import { Card, Icon } from "react-native-elements";
 
-const RenderMenu = ({ menu }) => {
+const RenderMenu = (props) => {
+  const { menu } = props;
   if (menu) {
     return (
-      <Card>
+      <Card style={styles.cardContainer}>
         <Card.Image source={menu.image}>
           <View style={styles.container}>
             <Text style={styles.name}>{menu.name}</Text>
@@ -12,6 +13,20 @@ const RenderMenu = ({ menu }) => {
         </Card.Image>
         <Text style={styles.description}>{menu.description}</Text>
         <Text style={styles.price}>{menu.price}</Text>
+        <View style={styles.cardRow}>
+          <Icon
+            name={props.isFavorite ? "check" : "shopping-cart"}
+            type="font-awesome"
+            color="#DB7093"
+            raised
+            reverse
+            onPress={() =>
+              props.isFavorite
+                ? console.log("Already placed in Cart")
+                : props.markFavorite()
+            }
+          />
+        </View>
       </Card>
     );
   }
@@ -22,6 +37,18 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: "center",
     flex: 1,
+  },
+  cardContainer: {
+    padding: 0,
+    margin: 0,
+    marginBottom: 20,
+  },
+  cardRow: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "row",
+    margin: 20,
   },
   name: {
     color: "white",
--- screens/MenuInfoScreen.js
+++ screens/MenuInfoScreen.js
@@ -1,7 +1,63 @@
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { COMMENTS } from "../shared/COMMENTS";
 import RenderMenu from "../features/menu/RenderMenu";
 
 const MenuInfoScreen = ({ route }) => {
   const { menu } = route.params;
-  return <RenderMenu menu={menu} />;
+
+  const [comments, setComments] = useState(COMMENTS);
+  const [favorite, setFavorite] = useState(false);
+
+  const renderCommentItem = ({ item }) => {
+    return (
+      <View style={styles.commentItem}>
+        <Text style={{ fontSize: 14 }}>{item.text}</Text>
+        <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+        <Text
+          style={{ fontSize: 12 }}
+        >{`-- ${item.author}, ${item.date}`}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <FlatList
+      data={comments.filter((comment) => comment.menuId === menu.id)}
+      renderItem={renderCommentItem}
+      keyExtractor={(item) => item.id.toString()}
+      contentContainerStyle={{
+        marginHorizontal: 20,
+        paddingVertical: 20,
+      }}
+      ListHeaderComponent={
+        <>
+          <RenderMenu
+            menu={menu}
+            isFavorite={favorite}
+            markFavorite={() => setFavorite(true)}
+          />
+          <Text style={styles.commentsTitle}>Comments</Text>
+        </>
+      }
+    />
+  );
 };
+
+const styles = StyleSheet.create({
+  commentsTitle: {
+    textAlign: "center",
+    backgroundColor: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#43484D",
+    padding: 10,
+    paddingTop: 30,
+  },
+  commentItem: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+  },
+});
 export default MenuInfoScreen;
