df56b5a7beaad9d60fc64e0fca3796f6b3e4844b
Zynkah <ffgurl07@yahoo.com>
2022-11-24 20:59:50 -0800

    Maids Updated

--- screens/MaidScreen.js
+++ screens/MaidScreen.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Text, ScrollView } from "react-native";
+import { Text, ScrollView, StyleSheet } from "react-native";
 import { Card, ListItem, Avatar } from "react-native-elements";
 import { MAIDS } from "../shared/MAIDS";
 
@@ -8,24 +8,28 @@ const MaidScreen = () => {
   return (
     <ScrollView>
       <Card wrapperStyle={{ margin: 20 }}>
-        <Card.Title>Our Mission</Card.Title>
+        <Card.Title style={styles.title}>Our Mission</Card.Title>
         <Card.Divider />
-        <Text>
+        <Text style={styles.mission}>
           Maid cafes focus on providing an escape from the home and work
-          spheres. Maids encapsulate a more pure form that provides an alternate
-          world to patrons.
+          spheres. Maids encapsulate a more pure form of service that provides
+          an alternate world to patrons.
         </Text>
       </Card>
       <Card>
-        <Card.Title>Our Maids</Card.Title>
+        <Card.Title style={styles.title}>Our Maids</Card.Title>
         <Card.Divider />
         {maids.map((maid) => {
           return (
             <ListItem key={maid.id}>
               <Avatar source={maid.image} size={100} />
               <ListItem.Content>
-                <ListItem.Title>{maid.name}</ListItem.Title>
-                <ListItem.Subtitle>{maid.description}</ListItem.Subtitle>
+                <ListItem.Title style={styles.title}>
+                  {maid.name}
+                </ListItem.Title>
+                <ListItem.Subtitle style={styles.description}>
+                  {maid.description}
+                </ListItem.Subtitle>
               </ListItem.Content>
             </ListItem>
           );
@@ -34,4 +38,20 @@ const MaidScreen = () => {
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    color: "#cc3366",
+    fontSize: 24,
+  },
+  description: {
+    fontSize: 16,
+    color: "#db7094",
+  },
+  mission: {
+    color: "#db7094",
+    fontSize: 16,
+    textAlign: "center",
+  },
+});
 export default MaidScreen;
